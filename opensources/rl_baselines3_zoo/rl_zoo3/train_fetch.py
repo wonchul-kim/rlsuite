f75fcd6e83a568d873dc80b5f92a6bf71426f6c3
wonchul <kim.wonchul@aiv.ai>
2025-08-21 17:37:04 +0900

    added m1013 env and train with rl-zoo

--- opensources/rl_baselines3_zoo/rl_zoo3/train_fetch.py
+++ opensources/rl_baselines3_zoo/rl_zoo3/train_fetch.py
@@ -22,8 +22,8 @@ def train() -> None:
     # parser.add_argument("--algo", help="RL Algorithm", default="ppo", type=str, required=False, choices=list(ALGOS.keys()))
     parser.add_argument("--algo", help="RL Algorithm", default="tqc", type=str, required=False, choices=list(ALGOS.keys()))
     # parser.add_argument("--env", type=str, default="CartPole-v1", help="environment ID")
-    # parser.add_argument("--env", type=str, default="FetchReach-v4", help="environment ID")
-    parser.add_argument("--env", type=str, default="M1013Env-v0", help="environment ID")
+    parser.add_argument("--env", type=str, default="FetchReach-v4", help="environment ID")
+    # parser.add_argument("--env", type=str, default="FetchReach-v1", help="environment ID")
     parser.add_argument("-tb", "--tensorboard-log", help="Tensorboard log dir", default="", type=str)
     parser.add_argument("-i", "--trained-agent", help="Path to a pretrained agent to continue training", default="", type=str)
     parser.add_argument(
@@ -177,12 +177,6 @@ def train() -> None:
     import gymnasium_robotics
     import gymnasium as gym
     gym.register_envs(gymnasium_robotics)
-    from rlsuite.envs.m1013.m1013_env import M1013Env
-    gym.register(
-        id="M1013Env-v0",
-        entry_point=M1013Env,
-        # max_episode_steps=100,  # Prevent infinite episodes
-    )
 
     env_id = args.env
     registered_envs = set(gym.envs.registry.keys())
